bdd407bd8030ae001709fa1303d54544011453b7
Seyi Ogunjuyigbe <seyiogunjuyigbe@gmail.com>
2019-07-24 15:30:16 +0100

    Google auth package installed, passport auth routes added

--- app.js
+++ app.js
@@ -1,7 +1,10 @@
 const express = require('express');
 const app = express();
 const bodyParser = require("body-parser");
-const mongoose = require("mongoose")
+const mongoose = require("mongoose");
+const passport = require('passport');
+const LocalStrategy = require('passport-local');
+const passportLocalMongoose = require('passport-local-mongoose');
 const methodOverride = require('method-override');
 
 
@@ -12,17 +15,24 @@ app.set("view engine", "ejs");
 app.use(bodyParser.urlencoded({extended: true}));
 app.use(express.static(__dirname + '/public'));
 
+
+//Schema Set Up
+//user schema setup
 const userSchema = new mongoose.Schema({
-    name: String,
+    firstName: String,
+    lastName: String,
     email: String,
     phone: Number,
+    username: String,
+    password: String,
     payments: [{
      amount: Number,
      date: String  
     }],
     balance: {type: Number, default: 0},
     category: String
-})
+});
+userSchema.plugin(passportLocalMongoose);
 let User = mongoose.model('User', userSchema);
 
 // User.remove({}, function(err){
@@ -32,9 +42,32 @@ let User = mongoose.model('User', userSchema);
 //     console.log('removed!')
 // }
 // })
+
+
+
+//PASSPORT CoNFIG
+app.use(require("express-session")({
+    secret: "cedar-secret",
+    resave: false,
+    saveUninitialized: false
+}));
+app.use(passport.initialize());
+app.use(passport.session());
+passport.use(new LocalStrategy(User.authenticate()));
+passport.serializeUser(User.serializeUser());
+passport.deserializeUser(User.deserializeUser());
+
+
+//ROUTES Setup
+
+
+//User Routes
+//New User
 app.get("/", function (req,res){
     res.render("new")
 })
+
+//Create new user
 app.post("/", function(req,res){
     let user = req.body.User;
     User.create(user, function(err, user){
@@ -46,24 +79,30 @@ app.post("/", function(req,res){
         }
     })
 })
+
+//New payment route
 app.post("/pay/:id", function(req,res){
     let payment = req.body.payments;
+    
     User.findById(req.params.id, function(err, thisUser){
         if(err){
             console.log(err)
         } else{
+            payment.date = new Date();
             thisUser.payments.push(payment);
             thisUser.save();
-            console.log(`${thisUser.name} paid ${payment.amount} on ${payment.date}`)
+            console.log(`${thisUser.firstName} paid ${payment.amount} on ${payment.date}`)
         }
         res.redirect("/profile/"+thisUser._id)
     })
+    
     
 
     
    
 })
 
+//Show User profile and balance
 app.get("/profile/:id", function(req,res){
     User.findById(req.params.id, function(err, thisUser){
         if(err){
@@ -83,12 +122,48 @@ app.get("/profile/:id", function(req,res){
     
 })
 
+
+//Show all users
+
 app.get("/all", function(req, res){
     User.find({}, function(err, allUsers){
         res.render("allUsers", {Users: allUsers})
     })
 })
 
+
+//Auth Routes
+app.get("/register", function(req,res){
+    res.render("register");
+});
+
+app.post("/register", function(req,res){
+    let newUser = new User({username: req.body.username})
+User.register(newUser, req.body.password, function(err,user){
+    if(err){
+        console.log(err);
+        return res.render('register');
+    } else{
+        passport.authenticate("local")(req, res, function(){
+            let user = req.body.user;
+            
+    User.create(user, function(err, user){
+        if(err){
+            console.log(err)
+        } else{
+            user.username = req.body.username;
+            user.password = req.body.password;
+            user.save();
+            console.log(user)
+            res.redirect("/profile/"+user._id)
+        }
+    })
+        })
+    }
+})
+
+
+})
 
 app.listen(8888, process.env.IP, function(){
     console.log("App Started!")
---
+++ notes.md
@@ -0,0 +1,2 @@
+Paystack Test Secret Key -  sk_test_96e638f48542c0be579ef8b0985b9955a413144a
+Paystack Test Public Key -  pk_test_b4754f3fd66d1d1de33960d0616ee0c33bb09137
